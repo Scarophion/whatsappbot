f96a3482953e1f75ab7bc0fb1561d40ebff2b347
Scarophion <scarophion@yahoo.com>
2026-06-10 10:03:41 +0100

    More stupid mistakes

--- server.js
+++ server.js
@@ -95,234 +95,239 @@ function createClient() {
         isReady = false;
     });
 
+    console.log('Initializing WhatsApp client...');
+    safeInitialize(client);
+}
+
+async function safeInitialize(client) {
     console.log('⏳ Waiting before initialization...');
     await new Promise(r => setTimeout(r, 5000));
 
-    console.log('Initializing WhatsApp client...');
     try {
         await client.initialize();
     } catch (err) {
         console.error('❌ Initialization failed:', err.message);
     }
+}
 
-    // function cleanSessionLocks() {
-    //     const sessionPath = '/data/whatsapp-session/session';
+// function cleanSessionLocks() {
+//     const sessionPath = '/data/whatsapp-session/session';
 
-    //     const lockFiles = [
-    //         'SingletonLock',
-    //         'SingletonCookie',
-    //         'SingletonSocket'
-    //     ];
+//     const lockFiles = [
+//         'SingletonLock',
+//         'SingletonCookie',
+//         'SingletonSocket'
+//     ];
 
-    //     lockFiles.forEach(file => {
-    //         const filePath = path.join(sessionPath, file);
+//     lockFiles.forEach(file => {
+//         const filePath = path.join(sessionPath, file);
 
-    //         if (fs.existsSync(filePath)) {
-    //             try {
-    //                 fs.unlinkSync(filePath);
-    //                 console.log(`🧹 Removed lock: ${file}`);
-    //             } catch (err) {
-    //                 console.log(`⚠️ Failed to remove ${file}`);
-    //             }
-    //         }
-    //     });
-    // }
+//         if (fs.existsSync(filePath)) {
+//             try {
+//                 fs.unlinkSync(filePath);
+//                 console.log(`🧹 Removed lock: ${file}`);
+//             } catch (err) {
+//                 console.log(`⚠️ Failed to remove ${file}`);
+//             }
+//         }
+//     });
+// }
 
-    function killChrome() {
-        try {
-            console.log('🧹 Killing existing Chrome processes...');
+function killChrome() {
+    try {
+        console.log('🧹 Killing existing Chrome processes...');
 
-            if (process.platform === 'win32') {
-                execSync('taskkill /F /IM chrome.exe /T', { stdio: 'ignore' });
-            } else {
-                execSync('pkill -f chromium || true');
-                execSync('pkill -f chrome || true');
-            }
-        } catch (err) {
-            console.log(err.message);
+        if (process.platform === 'win32') {
+            execSync('taskkill /F /IM chrome.exe /T', { stdio: 'ignore' });
+        } else {
+            execSync('pkill -f chromium || true');
+            execSync('pkill -f chrome || true');
         }
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+async function buildChatCache() {
+    console.log('📚 Building chat cache...');
+
+    const chats = await client.getChats();
+
+    chats.forEach(chat => {
+        chatCache.set(chat.name, chat.id._serialized);
+    });
+
+    console.log(`✅ Cached ${chatCache.size} chats`);
+}
+
+function isClientReady() {
+    console.log("Client readiness:", isReady, "Client object:", !!client, "Puppeteer page:", client ? !!client.pupPage : 'N/A',
+        "Puppeteer page open:", !client.pupPage.isClosed());
+    return isReady && client && client.pupPage && !client.pupPage.isClosed();
+}
+
+// HMAC verification
+function verifySignature(req) {
+    const signature = req.headers['x-signature'];
+    if (!signature) return false;
+
+    const body = JSON.stringify(req.body);
+
+    const expected = crypto
+        .createHmac('sha256', SECRET)
+        .update(body)
+        .digest('hex');
+
+    return signature === expected;
+}
+
+function checkRateLimit() {
+    const now = Date.now();
+    if (now - lastRequestTime < 2000) {
+        return res.status(429).send('Too many requests');
+    }
+    lastRequestTime = now;
+}
+
+async function checkClientReady() {
+    $ready = true;
+    $message = "";
+
+    if (!isReady) {
+        console.log('Client not ready.');
+        $ready = false;
+        $message = 'Client not ready, try again in a few seconds.';
+    }
+    else if (!client) {
+        console.log('Zombie state. WhatsApp client not initialized.');
+        ready = false;
+
+        if (!client.pupPage || client.pupPage.isClosed()) {
+            console.log('Puppeteer page not initialized. Destroying client.');
+            try {
+                await client.destroy();
+            }
+            catch (e) {
+                console.log('Error destroying client. Restart the app.:', e);
+            }
+        }
+
+        createClient();
+        message = 'Client restarting, try again in a few seconds.';
     }
 
-    async function buildChatCache() {
-        console.log('📚 Building chat cache...');
+    return { ready: $ready, message: $message };
+}
 
-        const chats = await client.getChats();
+// ====== API ENDPOINTS ======
+app.post('/send-message', async (req, res) => {
+    try {
+        if (!verifySignature(req)) {
+            return res.status(401).send('Unauthorized');
+        }
 
-        chats.forEach(chat => {
-            chatCache.set(chat.name, chat.id._serialized);
+        checkRateLimit();
+        isClientReady();
+        const { ready, respMessage } = await checkClientReady();
+
+        if (!ready) {
+            return res.status(503).send(respMessage);
+        }
+
+        const { group, message } = req.body;
+
+        if (!group || !message) {
+            return res.status(400).send('Missing group or message');
+        }
+
+        if (!chatCache || chatCache.size === 0) {
+            await buildChatCache();
+        }
+
+        const chatId = chatCache.get(group);
+
+        if (!chatId) {
+            return res.status(404).send('Group not found in cache');
+        }
+
+        await client.sendMessage(chatId, message);
+
+        console.log(`📤 Sent message to ${group} (${chatId})`);
+        res.send('Message sent');
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    } finally {
+        console.log("Exiting send-message endpoint");
+    }
+});
+
+app.post('/send-message-by-id', async (req, res) => {
+    try {
+        if (!verifySignature(req)) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        checkRateLimit();
+        isClientReady();
+        const { ready, respMessage } = await checkClientReady();
+
+        if (!ready) {
+            return res.status(503).send(respMessage);
+        }
+
+        const { chatId, message } = req.body;
+
+        if (!chatId || !message) {
+            return res.status(400).send('Missing chat ID or message');
+        }
+
+        await client.sendMessage(chatId, message);
+
+        console.log(`📤 Sent message to ${chatId}`);
+        res.send('Message sent');
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    } finally {
+        console.log("Exiting send-message endpoint");
+    }
+});
+
+// ====== START SERVER ======
+switch (HOST) {
+    case "localhost":
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`🚀 Server listening on port ${PORT}`);
+        });
+        break;
+    case "fly.io":
+        app.get('/', (req, res) => {
+            res.send('WhatsApp bot bumped.');
         });
 
-        console.log(`✅ Cached ${chatCache.size} chats`);
-    }
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`🚀 Server listening on port ${PORT}`);
+        });
+        break;
+    case "localhostssl":
+        // Certs
+        const options = {
+            pfx: fs.readFileSync(process.env.CERT_PATH),
+            passphrase: process.env.CERT_PASSWORD
+        };
+        // Server
+        https.createServer(options, app).listen(3000, () => {
+            console.log(`HTTPS server v on port ${PORT}`);
+        });
+        break;
+    default:
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`🚀 Server listening on port ${PORT}`);
+        });
+}
 
-    function isClientReady() {
-        console.log("Client readiness:", isReady, "Client object:", !!client, "Puppeteer page:", client ? !!client.pupPage : 'N/A',
-            "Puppeteer page open:", !client.pupPage.isClosed());
-        return isReady && client && client.pupPage && !client.pupPage.isClosed();
-    }
-
-    // HMAC verification
-    function verifySignature(req) {
-        const signature = req.headers['x-signature'];
-        if (!signature) return false;
-
-        const body = JSON.stringify(req.body);
-
-        const expected = crypto
-            .createHmac('sha256', SECRET)
-            .update(body)
-            .digest('hex');
-
-        return signature === expected;
-    }
-
-    function checkRateLimit() {
-        const now = Date.now();
-        if (now - lastRequestTime < 2000) {
-            return res.status(429).send('Too many requests');
-        }
-        lastRequestTime = now;
-    }
-
-    async function checkClientReady() {
-        $ready = true;
-        $message = "";
-
-        if (!isReady) {
-            console.log('Client not ready.');
-            $ready = false;
-            $message = 'Client not ready, try again in a few seconds.';
-        }
-        else if (!client) {
-            console.log('Zombie state. WhatsApp client not initialized.');
-            ready = false;
-
-            if (!client.pupPage || client.pupPage.isClosed()) {
-                console.log('Puppeteer page not initialized. Destroying client.');
-                try {
-                    await client.destroy();
-                }
-                catch (e) {
-                    console.log('Error destroying client. Restart the app.:', e);
-                }
-            }
-
-            createClient();
-            message = 'Client restarting, try again in a few seconds.';
-        }
-
-        return { ready: $ready, message: $message };
-    }
-
-    // ====== API ENDPOINTS ======
-    app.post('/send-message', async (req, res) => {
-        try {
-            if (!verifySignature(req)) {
-                return res.status(401).send('Unauthorized');
-            }
-
-            checkRateLimit();
-            isClientReady();
-            const { ready, respMessage } = await checkClientReady();
-
-            if (!ready) {
-                return res.status(503).send(respMessage);
-            }
-
-            const { group, message } = req.body;
-
-            if (!group || !message) {
-                return res.status(400).send('Missing group or message');
-            }
-
-            if (!chatCache || chatCache.size === 0) {
-                await buildChatCache();
-            }
-
-            const chatId = chatCache.get(group);
-
-            if (!chatId) {
-                return res.status(404).send('Group not found in cache');
-            }
-
-            await client.sendMessage(chatId, message);
-
-            console.log(`📤 Sent message to ${group} (${chatId})`);
-            res.send('Message sent');
-
-        } catch (err) {
-            console.error(err);
-            res.status(500).send('Server error');
-        } finally {
-            console.log("Exiting send-message endpoint");
-        }
-    });
-
-    app.post('/send-message-by-id', async (req, res) => {
-        try {
-            if (!verifySignature(req)) {
-                return res.status(401).send('Unauthorized');
-            }
-
-            checkRateLimit();
-            isClientReady();
-            const { ready, respMessage } = await checkClientReady();
-
-            if (!ready) {
-                return res.status(503).send(respMessage);
-            }
-
-            const { chatId, message } = req.body;
-
-            if (!chatId || !message) {
-                return res.status(400).send('Missing chat ID or message');
-            }
-
-            await client.sendMessage(chatId, message);
-
-            console.log(`📤 Sent message to ${chatId}`);
-            res.send('Message sent');
-
-        } catch (err) {
-            console.error(err);
-            res.status(500).send('Server error');
-        } finally {
-            console.log("Exiting send-message endpoint");
-        }
-    });
-
-    // ====== START SERVER ======
-    switch (HOST) {
-        case "localhost":
-            app.listen(PORT, '0.0.0.0', () => {
-                console.log(`🚀 Server listening on port ${PORT}`);
-            });
-            break;
-        case "fly.io":
-            app.get('/', (req, res) => {
-                res.send('WhatsApp bot bumped.');
-            });
-
-            app.listen(PORT, '0.0.0.0', () => {
-                console.log(`🚀 Server listening on port ${PORT}`);
-            });
-            break;
-        case "localhostssl":
-            // Certs
-            const options = {
-                pfx: fs.readFileSync(process.env.CERT_PATH),
-                passphrase: process.env.CERT_PASSWORD
-            };
-            // Server
-            https.createServer(options, app).listen(3000, () => {
-                console.log(`HTTPS server v on port ${PORT}`);
-            });
-            break;
-        default:
-            app.listen(PORT, '0.0.0.0', () => {
-                console.log(`🚀 Server listening on port ${PORT}`);
-            });
-    }
-
-    // ===== INITIALIZE =====
-    createClient();
+// ===== INITIALIZE =====
+createClient();
